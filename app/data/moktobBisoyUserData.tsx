5f8d0004abf64930fe661e7c7226d2b2f6b311a3
Jewel Rana <jewelranashepon@gmail.com>
2025-01-09 12:59:08 +0600

    Components and Api Created

---
+++ app/data/moktobBisoyUserData.tsx
@@ -0,0 +1,258 @@
+export const userMoktobBisoyData = {
+  "labelMap": {
+    "MoktobChalu": "নতুন মক্তব চালু হয়েছে",
+    "MoktobAdmit": "মক্তব থেকে ছাত্রছাত্রী মাদ্রাসায় ভর্তি হয়েছে",
+    "NewMoktob": "নতুন পুরাতন মোট মক্তব চালু আছে",
+    "Sikkha": "এই মাসে বয়স্ক কোরআন শিক্ষা চালু হয়েছে",
+    "TotalStudent": "মক্তবের মোট ছাত্র-ছাত্রীর সংখ্যা",
+    "TotalSikkha": "নতুন পুরাতন মোট বয়স্ক কোরআন চালু আছে",
+    "GurdianMeeting": "মক্তবের অভিভাবক সম্মেলন হয়েছে",
+    "TotalAgeSikkha": "বয়স্ক কোরআন শিক্ষা মোট অংশগ্রহণ করেছে",
+    "MadrasahAdmit": "মক্তব থেকে ছাত্রছাত্রী মাদ্রাসায় ভর্তি হয়েছে",
+    "NewMuslim": "নব মুসলিমদের দ্বীন শিক্ষার ফিকির করা হয়েছে"
+  },
+  "messi@gmail.com": {
+    "2025-01-01": {
+      "MoktobChalu": "3",
+      "MoktobAdmit": "2",
+      "NewMoktob": "3",
+      "Sikkha": "4",
+      "TotalStudent": "5",
+      "TotalSikkha": "6",
+      "GurdianMeeting": "7",
+      "TotalAgeSikkha": "8",
+      "MadrasahAdmit": "9",
+      "NewMuslim": "10"
+    },
+    "2025-01-02": {
+      "MoktobChalu": "20",
+      "MoktobAdmit": "150",
+      "NewMoktob": "4",
+      "Sikkha": "5",
+      "TotalStudent": "350",
+      "TotalSikkha": "6",
+      "GurdianMeeting": "2",
+      "TotalAgeSikkha": "20",
+      "MadrasahAdmit": "50",
+      "NewMuslim": "6"
+    },
+    "2025-01-03": {
+      "MoktobChalu": "8",
+      "MoktobAdmit": "300",
+      "NewMoktob": "4",
+      "Sikkha": "5",
+      "TotalStudent": "80",
+      "TotalSikkha": "6",
+      "GurdianMeeting": "2",
+      "TotalAgeSikkha": "20",
+      "MadrasahAdmit": "40",
+      "NewMuslim": "7"
+    },
+    "2025-01-04": {
+      "MoktobChalu": "7",
+      "MoktobAdmit": "90",
+      "NewMoktob": "9",
+      "Sikkha": "2",
+      "TotalStudent": "70",
+      "TotalSikkha": "8",
+      "GurdianMeeting": "6",
+      "TotalAgeSikkha": "8",
+      "MadrasahAdmit": "70",
+      "NewMuslim": "9"
+    },
+    "2025-01-05": {
+      "MoktobChalu": "2",
+      "MoktobAdmit": "4",
+      "NewMoktob": "3",
+      "Sikkha": "5",
+      "TotalStudent": "4",
+      "TotalSikkha": "4",
+      "GurdianMeeting": "5",
+      "TotalAgeSikkha": "4",
+      "MadrasahAdmit": "5",
+      "NewMuslim": "4"
+    },
+    "2025-01-06": {
+      "MoktobChalu": "50",
+      "MoktobAdmit": "100",
+      "NewMoktob": "150",
+      "Sikkha": "200",
+      "TotalStudent": "250",
+      "TotalSikkha": "300",
+      "GurdianMeeting": "350",
+      "TotalAgeSikkha": "400",
+      "MadrasahAdmit": "450",
+      "NewMuslim": "500"
+    }
+  },
+  "jewel@gmail.com": {
+    "2025-01-01": {
+      "MoktobChalu": "2",
+      "MoktobAdmit": "4",
+      "NewMoktob": "3",
+      "Sikkha": "5",
+      "TotalStudent": "4",
+      "TotalSikkha": "4",
+      "GurdianMeeting": "5",
+      "TotalAgeSikkha": "4",
+      "MadrasahAdmit": "5",
+      "NewMuslim": "4"
+    },
+    "2025-01-02": {
+      "MoktobChalu": "1",
+      "MoktobAdmit": "4",
+      "NewMoktob": "3",
+      "Sikkha": "5",
+      "TotalStudent": "4",
+      "TotalSikkha": "4",
+      "GurdianMeeting": "5",
+      "TotalAgeSikkha": "4",
+      "MadrasahAdmit": "5",
+      "NewMuslim": "4"
+    },
+    "2025-01-03": {
+      "MoktobChalu": "1",
+      "MoktobAdmit": "4",
+      "NewMoktob": "3",
+      "Sikkha": "5",
+      "TotalStudent": "4",
+      "TotalSikkha": "4",
+      "GurdianMeeting": "5",
+      "TotalAgeSikkha": "4",
+      "MadrasahAdmit": "5",
+      "NewMuslim": "4"
+    },
+    "2025-01-04": {
+      "MoktobChalu": "10",
+      "MoktobAdmit": "20",
+      "NewMoktob": "30",
+      "Sikkha": "40",
+      "TotalStudent": "50",
+      "TotalSikkha": "60",
+      "GurdianMeeting": "70",
+      "TotalAgeSikkha": "80",
+      "MadrasahAdmit": "90",
+      "NewMuslim": "100"
+    },
+    "2025-01-05": {
+      "MoktobChalu": "50",
+      "MoktobAdmit": "100",
+      "NewMoktob": "150",
+      "Sikkha": "200",
+      "TotalStudent": "250",
+      "TotalSikkha": "300",
+      "GurdianMeeting": "350",
+      "TotalAgeSikkha": "400",
+      "MadrasahAdmit": "450",
+      "NewMuslim": "500"
+    },
+    "2025-01-06": {
+      "MoktobChalu": "50",
+      "MoktobAdmit": "100",
+      "NewMoktob": "150",
+      "Sikkha": "200",
+      "TotalStudent": "250",
+      "TotalSikkha": "300",
+      "GurdianMeeting": "350",
+      "TotalAgeSikkha": "400",
+      "MadrasahAdmit": "450",
+      "NewMuslim": "500"
+    },
+    "2025-01-07": {
+      "MoktobChalu": "50",
+      "MoktobAdmit": "100",
+      "NewMoktob": "150",
+      "Sikkha": "200",
+      "TotalStudent": "250",
+      "TotalSikkha": "300",
+      "GurdianMeeting": "350",
+      "TotalAgeSikkha": "400",
+      "MadrasahAdmit": "450",
+      "NewMuslim": "500"
+    }
+  },
+  "faysal@gmail.com": {
+    "2025-01-06": {
+      "MoktobChalu": "50",
+      "MoktobAdmit": "100",
+      "NewMoktob": "150",
+      "Sikkha": "200",
+      "TotalStudent": "250",
+      "TotalSikkha": "300",
+      "GurdianMeeting": "350",
+      "TotalAgeSikkha": "400",
+      "MadrasahAdmit": "450",
+      "NewMuslim": "500"
+    }
+  },
+  "riyad@gmail.com": {
+    "2025-01-07": {
+      "MoktobChalu": "2",
+      "MoktobAdmit": "5",
+      "NewMoktob": "6",
+      "Sikkha": "74",
+      "TotalStudent": "5",
+      "TotalSikkha": "6",
+      "GurdianMeeting": "4",
+      "TotalAgeSikkha": "6",
+      "MadrasahAdmit": "7",
+      "NewMuslim": "6"
+    }
+  },
+  "nazmul@gmail.com": {
+    "2025-01-07": {
+      "MoktobChalu": "22",
+      "MoktobAdmit": "53",
+      "NewMoktob": "63",
+      "Sikkha": "74",
+      "TotalStudent": "5",
+      "TotalSikkha": "6",
+      "GurdianMeeting": "4",
+      "TotalAgeSikkha": "6",
+      "MadrasahAdmit": "7",
+      "NewMuslim": "6"
+    }
+  },
+  "sumon@gmail.com": {
+    "2025-01-07": {
+      "MoktobChalu": "100",
+      "MoktobAdmit": "200",
+      "NewMoktob": "300",
+      "Sikkha": "400",
+      "TotalStudent": "500",
+      "TotalSikkha": "600",
+      "GurdianMeeting": "700",
+      "TotalAgeSikkha": "800",
+      "MadrasahAdmit": "900",
+      "NewMuslim": "1000"
+    }
+  },
+  "moni@gmail.com": {
+    "2025-01-07": {
+      "MoktobChalu": "100",
+      "MoktobAdmit": "200",
+      "NewMoktob": "300",
+      "Sikkha": "400",
+      "TotalStudent": "500",
+      "TotalSikkha": "600",
+      "GurdianMeeting": "700",
+      "TotalAgeSikkha": "800",
+      "MadrasahAdmit": "900",
+      "NewMuslim": "1000"
+    }
+  },
+  "zisan@gmail.com": {
+    "2025-01-08": {
+      "MoktobChalu": "100",
+      "MoktobAdmit": "200",
+      "NewMoktob": "300",
+      "Sikkha": "400",
+      "TotalStudent": "500",
+      "TotalSikkha": "600",
+      "GurdianMeeting": "700",
+      "TotalAgeSikkha": "800",
+      "MadrasahAdmit": "900",
+      "NewMuslim": "1000"
+    }
+  }
+};
---
+++ app/data/talimBisoyUserData.tsx
@@ -0,0 +1,18 @@
+export const userTalimData = {
+  "labelMap": {
+    "mohilaTalim": "মহিলাদের মাঝে দ্বীনের তালিম",
+    "TalimOngshoGrohon": "মহিলাদের তালিমে মোট অংশগ্রহণ করেছে"
+  },
+  "jewel@gmail.com": {
+    "2025-01-06": {
+      "mohilaTalim": "10",
+      "TalimOngshoGrohon": "100"
+    }
+  },
+  "faysal@gmail.com": {
+    "2025-01-08": {
+      "mohilaTalim": "10",
+      "TalimOngshoGrohon": "100"
+    }
+  }
+};
