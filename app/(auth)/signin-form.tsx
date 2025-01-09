9f445e33410bd7fd7ba01546f0f0c540dbc4e4d3
estiak98 <estiakahmed898@gmail.com>
2025-01-09 12:31:30 +0600

    Update List
     -auth
     -signinform
     -admin page
     - Regestation
     - Notification
     - Combaind Dashboard
     - dynamic table

--- app/(auth)/signin-form.tsx
+++ app/(auth)/signin-form.tsx
@@ -28,31 +28,39 @@ export default function SigninForm() {
       router.push("/dashboard");
       alert("Invalid email or password");
     }
+    console.log("Sign-in Response:", res);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Email</label>
+      <div className="flex flex-col rounded-lg">
+        <label className="p-2 rounded-lg">Email</label>
         <input
           type="text"
           {...register("email")}
           placeholder="Enter your email"
+          className="p-2 rounded-lg"
         />
         {errors.email && <p>{errors.email.message}</p>}
       </div>
-      <div>
-        <label>Password</label>
+      <div className="flex flex-col ">
+        <label className="p-2 rounded-lg">Password</label>
         <input
           type="password"
           {...register("password")}
           placeholder="Enter your password"
+          className="p-2 rounded-lg"
         />
         {errors.password && <p>{errors.password.message}</p>}
       </div>
-      <button type="submit">Sign In</button>
+      <div className="align-middle">
+        <button
+          type="submit"
+          className="mt-5 py-3 px-5 bg-green-800 rounded-xl"
+        >
+          Sign In
+        </button>
+      </div>
     </form>
   );
 }
-
-
--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,11 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
 // import bcrypt from "bcryptjs";
 import { signInSchema } from "@/validators/authValidators";
 import { getUserByEmail } from "@/lib/getData";
 
 export default {
   providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    Github({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = await signInSchema.validate(credentials);
@@ -26,9 +16,10 @@ export default {
         console.log(user, "Hello");
         if (!user || !user.password) return null;
 
-        const passwordMatch = password == user.password;
+        const passwordMatch = password === user.password;
 
         if (passwordMatch) return user;
+        console.log(user, "Hello");
 
         return null;
       },
