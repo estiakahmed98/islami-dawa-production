572fb962127a28fa2d4ccfe65a64ad03b6f56ffd
estiak98 <estiakahmed898@gmail.com>
2025-02-18 11:44:25 +0600

    Darkmode off from tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class"],
+  // darkMode: ["class"],
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
