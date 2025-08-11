7e42aed411a963f49d1571e795915bbe8abf49d1
estiakahmed98 <estiakahmed898@gmail.com>
2025-08-11 11:42:21 +0600

    Login Updated

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,14 +6,16 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="grid h-screen place-items-center px-4 py-8">
       <main className="flex w-full max-w-sm flex-col gap-8">
-        <Image
-          src="/boed.png"
-          width={200}
+      <div className="flex items-center justify-center bg-[#155E75] p-2 rounded-lg">
+       <Image
+          src="/logo_img.png"
+          width={100}
           height={100}
           alt="logo"
-          className="mx-auto w-auto"
           priority
         />
+        <h1 className="text-xl font-bold text-center text-white">ইসলামি দাওয়াহ ইনস্টিটিউট বাংলাদেশ </h1>
+       </div>
         {children}
       </main>
     </div>
